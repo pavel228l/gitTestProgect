d77d18cfeb7831960b60f8f2160f2c928a9f3954
pavel228l <baklazhanchik22@gmail.com>
2025-02-13 09:50:08 +0300

    иуииууиу

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -20,6 +20,7 @@ public class MainTest {
         System.out.println("belki raidery");
         System.out.println("krokodily gorilly");
         System.out.println("bobry");
+        System.out.println("bobry");
 
     }
       @Test
