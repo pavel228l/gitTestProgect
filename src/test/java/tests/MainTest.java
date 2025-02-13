9f604ba4eb160c01c6c4515e1d40e891e848e74e
pavel228l <baklazhanchik22@gmail.com>
2025-02-13 09:50:46 +0300

    Update MainTest.java

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -20,9 +20,8 @@ public class MainTest {
         System.out.println("belki raidery");
         System.out.println("krokodily gorilly");
         System.out.println("bobry");
-        System.out.println("bobry");
-
     }
+    
       @Test
     public void bebebe3() {
        System.out.println("pingvin zombi");
